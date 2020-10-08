6c7b6dd607516beb0075b7f3bd68fa8d2cf5c21d
buraksekili <buraksekili@gmail.com>
2020-10-08 20:59:50 +0300

    Added context api for server response

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ConnectionForm from "./ConnectionForm";
 import QueryForm from "./QueryForm";
+import { ResponseContext } from "./ResponseContext";
 import Tables from "./Tables";
 
 export default function App() {
+  const [response, setResponse] = useState(undefined);
+
   return (
     <>
-      <ConnectionForm />
-      <QueryForm />
-      <Tables />
+      <ResponseContext.Provider value={{ response, setResponse }}>
+        <ConnectionForm />
+        <QueryForm />
+        <Tables />
+      </ResponseContext.Provider>
     </>
   );
 }
