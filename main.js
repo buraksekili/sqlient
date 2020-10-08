7c8d842d209fab8c9a6b50f1c58fc1018bb40d41
buraksekili <buraksekili@gmail.com>
2020-10-08 17:08:05 +0300

    Initial commit for electron-react boilerplate

---
+++ main.js
@@ -0,0 +1,51 @@
+const { BrowserWindow, app, ipcMain, Notification } = require("electron");
+const path = require("path");
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile("index.html");
+  win.webContents.openDevTools();
+}
+
+// if (isDev) {
+//   require("electron-reload")(__dirname, {
+//     electron: path.join(__dirname, "node_modules", ".bin", "electron"),
+//   });
+// }
+
+// ipcMain.on("notify", (_, message) => {
+//   new Notification({ title: "Notifiation", body: message }).show();
+// });
+
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// for hot module refreshing
+try {
+  require("electron-reloader")(module);
+} catch (_) {}
