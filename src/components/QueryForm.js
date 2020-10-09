a3f128f8e7dad08498229d2323db79c1e1826b13
buraksekili <buraksekili@gmail.com>
2020-10-09 09:32:41 +0300

    Added query execution in client

--- src/components/QueryForm.js
+++ src/components/QueryForm.js
@@ -1,10 +1,36 @@
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import React from "react";
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { ConnectionContext } from "./ConnectionContext";
+import { ResponseContext } from "./ResponseContext";
 
 const buttonCSS = { marginLeft: 8, marginRight: 8, marginTop: 8, width: "15%" };
 
 const QueryForm = () => {
+  const [query, setQuery] = useState(undefined);
+  const [helper, setHelper] = useState(undefined);
+  const { connectionInfo } = useContext(ConnectionContext);
+  const { response } = useContext(ResponseContext);
+
+  // insert into product (productName,productPrice, productStock) values ('yeni',20, 50)
+
+  const executeQuery = () => {
+    const data = { ...connectionInfo, query };
+
+    axios
+      .post("http://localhost:5000/execute", { data })
+      .then((res) => {
+        if (res.data.error) {
+          setHelper(res.data.error);
+          return;
+        } else if (res.data.status) {
+          setHelper("Executed!");
+        }
+      })
+      .catch((e) => console.log("eeee", e));
+  };
+
   return (
     <div className="query-form">
       <TextField
@@ -12,16 +38,23 @@ const QueryForm = () => {
         label="Query"
         style={{ margin: 8, width: "80%" }}
         placeholder="Your query:"
-        // fullWidth
         margin="normal"
         InputLabelProps={{
           shrink: true,
         }}
         variant="outlined"
+        helperText={helper && helper}
+        onChange={(e) => setQuery(e.target.value)}
       />
-      <Button style={buttonCSS} variant="contained">
-        Execute
-      </Button>
+      {response && query && (
+        <Button
+          style={buttonCSS}
+          onClick={() => executeQuery()}
+          variant="contained"
+        >
+          Execute
+        </Button>
+      )}
     </div>
   );
 };
